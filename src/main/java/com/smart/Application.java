bc93853db1bc60a3ad73740d672423d1e6f69f79
BG235145 <414511018@qq.com>
2022-07-05 00:08:11 +0800

    1

--- src/main/java/com/smart/Application.java
+++ src/main/java/com/smart/Application.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.WebApplicationInitializer;
 
 @SpringBootApplication
 @EnableAsync
-@EnableTransactionManagement
 public class Application extends SpringBootServletInitializer implements WebApplicationInitializer {
 
     public static void main(String[] args) {
